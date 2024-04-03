5fb24cdd457eafde3d4acdd983c61740a12e726f
waka____ <12706753+waka-waka-waka-waka@users.noreply.github.com>
2024-04-03 13:08:55 -0700

    More formatting

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const startTime = 1708977600;
 let minotaursKilled = 0;
 
 async function fetchPlayerEXP() {
+  const currentTime = Date.now();
+  if (cachedData && currentTime - cacheTimestamp < CACHE_DURATION) {
+    console.log('Using cached data');
+    return cachedData;
+  }
+
   const apiUrl = `https://osrs-hiscore-pulling.onrender.com/stats/OneChunkUp?skill=ranged`;
-  
+
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log('API Response:', data);
+    cachedData = data.experience;
+    cacheTimestamp = currentTime;
     return data.experience;
   } catch (error) {
     console.error('Error fetching player EXP:', error);
@@ -49,9 +57,9 @@ async function updateResult() {
     const resultElement = document.getElementById('result');
     resultElement.innerHTML = `
       <p>OneChunkUp has killed approximately ${totalMinotaursKilled} Minotaurs.</p>
-      <p>Minotaurs killed per minute: ${minotaursPerMinute.toFixed(2)}</p>
-      <p>Minotaurs killed per day: ${Math.floor(minotaursPerDay)}</p>
-      <p>Estimated Pure Rune Essence obtained: ${pureRuneEssence}</p>
+      <p><img src="Ensouled_minotaur_head.webp" alt="Ensouled Minotaur Head"> Minotaurs killed per minute: ${minotaursPerMinute.toFixed(2)}</p>
+      <p><img src="Ensouled_minotaur_head.webp" alt="Ensouled Minotaur Head"> Minotaurs killed per day: ${Math.floor(minotaursPerDay)} (Starting February 26th, 2024)</p>
+      <p><img src="Pure_essence.webp" alt="Pure Essence"> Estimated Pure Rune Essence obtained: ${pureRuneEssence}</p>
     `;
   } else {
     console.error('Failed to fetch player EXP.');
@@ -63,27 +71,5 @@ async function updateResult() {
 const CACHE_DURATION = 600000; // Cache for 10 minutes
 let cachedData = null;
 let cacheTimestamp = 0;
-
-async function fetchPlayerEXP() {
-  const currentTime = Date.now();
-  if (cachedData && currentTime - cacheTimestamp < CACHE_DURATION) {
-    console.log('Using cached data');
-    return cachedData;
-  }
-
-  const apiUrl = `https://osrs-hiscore-pulling.onrender.com/stats/OneChunkUp?skill=ranged`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log('API Response:', data);
-    cachedData = data.experience;
-    cacheTimestamp = currentTime;
-    return data.experience;
-  } catch (error) {
-    console.error('Error fetching player EXP:', error);
-    return null;
-  }
-}
 
 updateResult();
